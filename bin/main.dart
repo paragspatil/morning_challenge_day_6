07e6aeaba0c460cd53bb02f582422df90d7b8aa7
Parag <paragspatil123@gmail.com>
2019-10-07 10:54:25 +0530

    day 6 startup complete

--- bin/main.dart
+++ bin/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+import 'dart:io';
 // Challenge 1
 // A number is a perfect number if is equal to sum of its proper divisors, that is, sum of its positive divisors excluding the number itself.
 // Write a function to check if a given number is perfect or not.
@@ -5,6 +7,21 @@
 // divisors is 9 which is not equal to 15
 // if 6 is given as input it should print 'true' because Divisors of 6 are 1, 2 and 3. Sum of
 // divisors is 6.
+void PerfectNumber(int m) {
+  int temp = 0;
+  int i = 0;
+  for (i = 0; i < m~/2; i++);{
+   if(m%i==0) {
+     temp = temp + i;
+   }
+  }
+  if(temp==m) {
+    print('$m is a perfect number ');
+  }
+    else {
+    print('$m is not a perfect number');
+  }
+}
 
 class Point {
   int x;
@@ -22,12 +39,42 @@ class Point {
 // For example, given the points (-1, 0), (2, 2), (1,3) the function should print:
 // Bottom-left: (-1, 0)
 // Top-right: (2,3)
+void lowbound(List <Point> h) {
+  int l = h.length;
+  int i = 0;
+  List xaxis = [];
+  List yaxis = [];
+  int x = 0;
+  int y = 0;
+  for (i = 0; i < l - 1; i++) {
+    Point p = h[i];
+    int x = p.x;
+    int y = p.y;
+    xaxis.add(x);
+    yaxis.add(y);
+  }
+  int j = 1;
+  while (j != l - 1) {
+    int xmax = max(xaxis[j-1], xaxis[j]);
+    int xmin = min(xaxis[j-1], xaxis[j]);
+    int ymax = max(yaxis[j-1], yaxis[j]);
+    int ymin = min(yaxis[j-1], yaxis[j]);
+    j = j + 1;
+    print('coordinates of bottom left point are ( $xmin , $ymin  )');
+    print('coordinates of bottom left point are ( $xmax , $xmax  )');
+  }
+}
+  
+
 
 
 
 void main() {
   // Test the function defined above with given List of points
   List<Point> points = [Point(-1, 0), Point(2, 2), Point(1, 3)];
+lowbound(points);
+  PerfectNumber(13);
+
 
   // Test the perfect number function here
 
